d9ce5c851ef60ce2fd1b6e1e0a35281f0ca0e004
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-05-26 21:23:05 +0200

    Misctypes reorg.

--- serialize/ser_misctypes.ml
+++ serialize/ser_misctypes.ml
@@ -18,36 +18,19 @@
 (**********************************************************************)
 
 open Sexplib.Std
-open Sexplib.Sexp
 
 open Ser_names
 open Ser_loc
-
-open Misctypes
-
-(* type 'a glob_sort_gen = GProp | GSet | GType of 'a *)
-
-type glob_level = [%import: Misctypes.glob_level]
-let glob_level_of_sexp _x = GProp
-let sexp_of_glob_level _x = Atom ""
-
-type glob_sort = [%import: Misctypes.glob_sort]
-let glob_sort_of_sexp _x = GProp
-let sexp_of_glob_sort _x = Atom ""
 
 type patvar = [%import: Misctypes.patvar]
 let patvar_of_sexp = id_of_sexp
 let sexp_of_patvar = sexp_of_id
 
-type 'a cast_type = [%import: 'a Misctypes.cast_type]
-  [@@deriving sexp]
-
-type case_style = [%import: Misctypes.case_style]
-                [@@deriving sexp]
-
-type 'constr intro_pattern_expr = [%import: 'constr Misctypes.intro_pattern_expr
-                                  [@with Names.Id.t := id;
-                                  ]]
+type 'constr intro_pattern_expr =
+  [%import: 'constr Misctypes.intro_pattern_expr
+              [@with
+                Names.Id.t := id;
+              ]]
 and intro_pattern_naming_expr   = [%import: Misctypes.intro_pattern_naming_expr
                                   [@with Names.Id.t := id;
                                   ]]
@@ -57,9 +40,103 @@ and 'constr intro_pattern_action_expr = [%import: 'constr Misctypes.intro_patter
 and 'constr or_and_intro_pattern_expr = [%import: 'constr Misctypes.or_and_intro_pattern_expr
                                   [@with Loc.t := loc;
                                   ]]
-[@@deriving sexp]
+  [@@deriving sexp]
 
-type 'a or_by_notation = [%import: 'a Misctypes.or_by_notation
-    [@with Loc.t := loc;
-    ]]
-[@@deriving sexp]
+type 'id move_location =
+  [%import: 'id Misctypes.move_location]
+  [@@deriving sexp]
+
+type 'a glob_sort_gen =
+  [%import: 'a Misctypes.glob_sort_gen]
+  [@@deriving sexp]
+
+type sort_info =
+  [%import: Misctypes.sort_info
+  [@with
+    Loc.t       := loc;
+    Loc.located := located;
+  ]]
+  [@@deriving sexp]
+
+type level_info =
+  [%import: Misctypes.level_info
+  [@with
+    Loc.t       := loc;
+    Loc.located := located;
+  ]]
+  [@@deriving sexp]
+
+type glob_sort =
+  [%import: Misctypes.glob_sort]
+  [@@deriving sexp]
+
+type glob_level =
+  [%import: Misctypes.glob_level]
+  [@@deriving sexp]
+
+type case_style =
+  [%import: Misctypes.case_style]
+  [@@deriving sexp]
+
+type 'a cast_type = [%import: 'a Misctypes.cast_type]
+  [@@deriving sexp]
+
+type quantified_hypothesis =
+  [%import: Misctypes.quantified_hypothesis
+  [@with
+    Loc.t := loc;
+    Names.Id.t := id;
+  ]]
+  [@@deriving sexp]
+
+type 'a explicit_bindings =
+  [%import: 'a Misctypes.explicit_bindings
+  [@with
+    Loc.t := loc;
+  ]]
+  [@@deriving sexp]
+
+type 'a bindings =
+  [%import: 'a Misctypes.bindings
+  [@with
+    Loc.t := loc;
+    Names.Id.t := id;
+  ]]
+  [@@deriving sexp]
+
+type 'a with_bindings =
+  [%import: 'a Misctypes.with_bindings
+  [@with
+    Loc.t := loc;
+  ]]
+  [@@deriving sexp]
+
+type 'a or_var =
+  [%import: 'a Misctypes.or_var
+  [@with
+    Loc.t := loc;
+    Loc.located := located;
+    Names.Id.t := id;
+  ]]
+  [@@deriving sexp]
+
+type 'a and_short_name =
+  [%import: 'a Misctypes.and_short_name
+  [@with
+    Loc.t := loc;
+    Loc.located := located;
+    Names.Id.t := id;
+  ]]
+  [@@deriving sexp]
+
+type 'a or_by_notation =
+  [%import: 'a Misctypes.or_by_notation
+  [@with
+    Loc.t := loc;
+  ]]
+  [@@deriving sexp]
+
+type module_kind =
+  [%import: Misctypes.module_kind]
+  [@@deriving sexp]
+
--- serialize/ser_misctypes.mli
+++ serialize/ser_misctypes.mli
@@ -19,31 +19,14 @@
 
 open Sexplib
 
-type glob_level                       = Misctypes.glob_level
-type glob_sort                        = Misctypes.glob_sort
-type patvar                           = Misctypes.patvar
-type 'a cast_type                     = 'a Misctypes.cast_type
-type case_style                       = Misctypes.case_style
+type patvar = Names.Id.t
+val patvar_of_sexp : Sexp.t -> Names.Id.t
+val sexp_of_patvar : Names.Id.t -> Sexp.t
 
-type 'constr intro_pattern_expr        = 'constr Misctypes.intro_pattern_expr
-type intro_pattern_naming_expr         = Misctypes.intro_pattern_naming_expr
-type 'constr intro_pattern_action_expr = 'constr Misctypes.intro_pattern_action_expr
-type 'constr or_and_intro_pattern_expr = 'constr Misctypes.or_and_intro_pattern_expr
-
-val glob_level_of_sexp : 'a -> 'b Misctypes.glob_sort_gen
-val sexp_of_glob_level : 'a -> Sexp.t
-
-val glob_sort_of_sexp : 'a -> 'b Misctypes.glob_sort_gen
-val sexp_of_glob_sort : 'a -> Sexp.t
-
-val patvar_of_sexp : Sexp.t -> Misctypes.patvar
-val sexp_of_patvar : Misctypes.patvar -> Sexp.t
-
-val cast_type_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a cast_type
-val sexp_of_cast_type : ('a -> Sexp.t) -> 'a cast_type -> Sexp.t
-
-val case_style_of_sexp : Sexp.t -> case_style
-val sexp_of_case_style : case_style -> Sexp.t
+type 'constr intro_pattern_expr = 'constr Misctypes.intro_pattern_expr
+and intro_pattern_naming_expr   = Misctypes.intro_pattern_naming_expr
+and 'constr intro_pattern_action_expr = 'constr Misctypes.intro_pattern_action_expr
+and 'constr or_and_intro_pattern_expr = 'constr Misctypes.or_and_intro_pattern_expr
 
 val intro_pattern_expr_of_sexp : (Sexp.t -> 'constr) -> Sexp.t -> 'constr intro_pattern_expr
 val sexp_of_intro_pattern_expr : ('constr -> Sexp.t) -> 'constr intro_pattern_expr -> Sexp.t
@@ -57,7 +40,78 @@ val sexp_of_intro_pattern_action_expr : ('constr -> Sexp.t) -> 'constr intro_pat
 val or_and_intro_pattern_expr_of_sexp : (Sexp.t -> 'constr) -> Sexp.t -> 'constr or_and_intro_pattern_expr
 val sexp_of_or_and_intro_pattern_expr : ('constr -> Sexp.t) -> 'constr or_and_intro_pattern_expr -> Sexp.t
 
+type 'id move_location = 'id Misctypes.move_location
+
+val move_location_of_sexp : (Sexp.t -> 'id) -> Sexp.t -> 'id move_location
+val sexp_of_move_location : ('id -> Sexp.t) -> 'id move_location -> Sexp.t
+
+type 'a glob_sort_gen = 'a Misctypes.glob_sort_gen
+
+val glob_sort_gen_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a glob_sort_gen
+val sexp_of_glob_sort_gen : ('a -> Sexp.t) -> 'a glob_sort_gen -> Sexp.t
+
+type sort_info = Misctypes.sort_info
+val sort_info_of_sexp : Sexp.t -> sort_info
+val sexp_of_sort_info : sort_info -> Sexp.t
+
+type level_info = Misctypes.level_info
+val level_info_of_sexp : Sexp.t -> level_info
+val sexp_of_level_info : level_info -> Sexp.t
+
+type glob_sort = Misctypes.glob_sort
+val glob_sort_of_sexp : Sexp.t -> glob_sort
+val sexp_of_glob_sort : glob_sort -> Sexp.t
+
+type glob_level = Misctypes.glob_level
+val glob_level_of_sexp : Sexp.t -> glob_level
+val sexp_of_glob_level : glob_level -> Sexp.t
+
+type case_style = Term.case_style
+
+val case_style_of_sexp : Sexp.t -> case_style
+val sexp_of_case_style : case_style -> Sexp.t
+
+type 'a cast_type = 'a Misctypes.cast_type
+
+val cast_type_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a cast_type
+val sexp_of_cast_type : ('a -> Sexp.t) -> 'a cast_type -> Sexp.t
+
+type quantified_hypothesis = Misctypes.quantified_hypothesis
+
+val quantified_hypothesis_of_sexp : Sexp.t -> quantified_hypothesis
+val sexp_of_quantified_hypothesis : quantified_hypothesis -> Sexp.t
+
+type 'a explicit_bindings = 'a Misctypes.explicit_bindings
+
+val explicit_bindings_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a explicit_bindings
+val sexp_of_explicit_bindings : ('a -> Sexp.t) -> 'a explicit_bindings -> Sexp.t
+
+type 'a bindings = 'a Misctypes.bindings
+
+val bindings_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a bindings
+val sexp_of_bindings : ('a -> Sexp.t) -> 'a bindings -> Sexp.t
+
+type 'a with_bindings = 'a Misctypes.with_bindings
+
+val with_bindings_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a with_bindings
+val sexp_of_with_bindings : ('a -> Sexp.t) -> 'a with_bindings -> Sexp.t
+
+type 'a or_var = 'a Misctypes.or_var
+
+val or_var_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a or_var
+val sexp_of_or_var : ('a -> Sexp.t) -> 'a or_var -> Sexp.t
+
+type 'a and_short_name = 'a Misctypes.and_short_name
+
+val and_short_name_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a and_short_name
+val sexp_of_and_short_name : ('a -> Sexp.t) -> 'a and_short_name -> Sexp.t
+
 type 'a or_by_notation = 'a Misctypes.or_by_notation
 
-val or_by_notation_of_sexp : (Sexplib.Sexp.t -> 'a) -> Sexplib.Sexp.t -> 'a or_by_notation
-val sexp_of_or_by_notation : ('a -> Sexplib.Sexp.t) -> 'a or_by_notation -> Sexplib.Sexp.t
+val or_by_notation_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a or_by_notation
+val sexp_of_or_by_notation : ('a -> Sexp.t) -> 'a or_by_notation -> Sexp.t
+
+type module_kind = Misctypes.module_kind
+val module_kind_of_sexp : Sexp.t -> module_kind
+val sexp_of_module_kind : module_kind -> Sexp.t
+
