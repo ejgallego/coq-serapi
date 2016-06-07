bcfc4d69b832ab64d220ac4848c46204b4e796dd
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-06-07 02:18:04 +0200

    Remove constructor from stateid

--- serialize/ser_stateid.ml
+++ serialize/ser_stateid.ml
@@ -18,10 +18,11 @@ open Sexplib.Std
 (* Id: private *)
 type stateid   = [%import: Stateid.t]
 
-type _stateid                = Ser_Stateid of int [@@deriving sexp]
+type _stateid                = int [@@deriving sexp]
+(* type _stateid             = Ser_Stateid of int [@@deriving sexp] *)
 
-let _stateid_put stateid               = Ser_Stateid (Stateid.to_int stateid)
-let _stateid_get (Ser_Stateid stateid) = Stateid.of_int stateid
+let _stateid_put stateid = (Stateid.to_int stateid)
+let _stateid_get stateid = Stateid.of_int stateid
 
 let stateid_of_sexp sexp    = _stateid_get (_stateid_of_sexp sexp)
 let sexp_of_stateid stateid = sexp_of__stateid (_stateid_put stateid)
