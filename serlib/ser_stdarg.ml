9306cbb87e73f98c467bee80b49d50475b1ef574
Emilio Jesus Gallego Arias <e+git@x80.org>
2018-02-19 08:25:20 +0100

    [serlib] Add more missing genargs.

--- serlib/ser_stdarg.ml
+++ serlib/ser_stdarg.ml
@@ -13,12 +13,35 @@
 (* Status: Very Experimental                                            *)
 (************************************************************************)
 
+let mk_unif p = Ser_genarg.{
+    raw_ser = p;
+    glb_ser = p;
+    top_ser = p;
+  }
+
+let ser_wit_int    = mk_unif Sexplib.Conv.sexp_of_int
+let ser_wit_bool   = mk_unif Sexplib.Conv.sexp_of_bool
+let ser_wit_string = mk_unif Sexplib.Conv.sexp_of_string
+let ser_wit_ident  = mk_unif Ser_names.Id.sexp_of_t
+
 let ser_wit_constr = Ser_genarg.{
     raw_ser = Ser_constrexpr.sexp_of_constr_expr;
     glb_ser = Ser_tactypes.sexp_of_glob_constr_and_expr;
     top_ser = Ser_eConstr.sexp_of_t;
   }
 
+let ser_wit_uconstr = Ser_genarg.{
+    raw_ser = Ser_constrexpr.sexp_of_constr_expr;
+    glb_ser = Ser_tactypes.sexp_of_glob_constr_and_expr;
+    top_ser = Ser_glob_term.sexp_of_closed_glob_constr;
+  }
+
 let register () =
-  Ser_genarg.register_genser Stdarg.wit_constr ser_wit_constr
+  Ser_genarg.register_genser Stdarg.wit_string ser_wit_string;
+  Ser_genarg.register_genser Stdarg.wit_int ser_wit_int;
+  Ser_genarg.register_genser Stdarg.wit_bool ser_wit_bool;
+  Ser_genarg.register_genser Stdarg.wit_ident ser_wit_ident;
+
+  Ser_genarg.register_genser Stdarg.wit_constr ser_wit_constr;
+  Ser_genarg.register_genser Stdarg.wit_uconstr ser_wit_uconstr
 
