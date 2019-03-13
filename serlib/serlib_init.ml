d83d44909e6697da715ae91b500841efcfecd962
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-03-13 19:20:39 +0100

    [serlib] Turn serialization options into a field.

--- serlib/serlib_init.ml
+++ serlib/serlib_init.ml
@@ -1,5 +1,11 @@
-let init ~omit_loc ~omit_att ~exn_on_opaque =
-  Ser_loc.omit_loc  := omit_loc;
-  Ser_cAst.omit_att := omit_att;
-  Serlib_base.exn_on_opaque := exn_on_opaque;
+type options =
+  { omit_loc : bool
+  ; omit_att : bool
+  ; exn_on_opaque : bool
+  }
+
+let init ~options =
+  Ser_loc.omit_loc  := options.omit_loc;
+  Ser_cAst.omit_att := options.omit_att;
+  Serlib_base.exn_on_opaque := options.exn_on_opaque;
   ()
--- serlib/serlib_init.mli
+++ serlib/serlib_init.mli
@@ -1,1 +1,8 @@
-val init : omit_loc:bool -> omit_att:bool -> exn_on_opaque:bool -> unit
+type options =
+  { omit_loc : bool
+  ; omit_att : bool
+  ; exn_on_opaque : bool
+  }
+
+val init : options:options -> unit
+
