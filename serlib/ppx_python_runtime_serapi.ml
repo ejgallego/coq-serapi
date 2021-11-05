d26b0f02c6b12b52f270a4d1a6df45a3678bc4d6
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-11-05 02:20:36 +0100

    fixup for 4.07.1

--- serlib/ppx_python_runtime_serapi.ml
+++ serlib/ppx_python_runtime_serapi.ml
@@ -2,7 +2,7 @@ include Ppx_python_runtime_pycoq
 include Ppx_python_runtime_pycoq.Ppx_python_runtime
 
 (* To remove in newer ppx_python release *)
-exception Not_found_s = Base.Not_found_s
+(* exception Not_found_s = Base.Not_found_s *)
 
 let python_of_unit _ = Py.Int.of_int 0
 let unit_of_python _ = ()
