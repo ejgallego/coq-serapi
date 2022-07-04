a8ed37d0ee6feaac0196aa286188190c26916b08
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-07-05 00:16:51 +0200

    [wip] Prepare for module access.

--- serlib/ser_declaremods.ml
+++ serlib/ser_declaremods.ml
@@ -19,6 +19,9 @@ open Ppx_hash_lib.Std.Hash.Builtin
 open Ppx_compare_lib.Builtin
 open Sexplib.Conv
 
+module Nametab = Ser_nametab
+module Libobject = Ser_libobject
+
 type 'a module_signature =
   [%import: 'a Declaremods.module_signature]
   [@@deriving sexp,yojson,hash,compare]
@@ -26,3 +29,10 @@ type 'a module_signature =
 type inline =
   [%import: Declaremods.inline]
   [@@deriving sexp,yojson,hash,compare]
+
+type _module_objects =
+  { module_prefix : Nametab.object_prefix;
+    module_substituted_objects : Libobject.t list;
+    module_keep_objects : Libobject.t list;
+  } [@@deriving sexp]
+
