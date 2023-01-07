afd202052fbcbe1089d35c8ca89d43e7abb24f48
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-01-07 21:38:48 +0100

    [serlib] Support number_string_notation_plugin

--- sertop/sertop_loader.ml
+++ sertop/sertop_loader.ml
@@ -32,6 +32,7 @@ let map_serlib fl_pkg =
     | "coq-core.plugins.extraction"       (* setoid_ring *)
     | "coq-core.plugins.ssrmatching"      (* ssrmatching *)
     | "coq-core.plugins.ssreflect"        (* ssr *)
+    | "coq-core.plugins.number_string_notation" (* syntax *)
       -> true
     | _ ->
       if debug then Format.eprintf "missing serlib: %s@\n%!" fl_pkg;
