02d287c413d8440eda2abea4eecddee11971e709
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-02-07 00:41:30 +0100

    [coq] Compatibility with https://github.com/coq/coq/pull/17219

--- serlib/plugins/syntax/ser_g_number_syntax.ml
+++ serlib/plugins/syntax/ser_g_number_syntax.ml
@@ -7,17 +7,6 @@ open Ppx_hash_lib.Std.Hash.Builtin
 module Libnames = Ser_libnames
 module Notation = Ser_notation
 
-module A1 = struct
-  type raw = Notation.numnot_option
-  [@@deriving sexp,hash,compare]
-  type glb = unit
-  [@@deriving sexp,hash,compare]
-  type top = unit
-  [@@deriving sexp,hash,compare]
-end
-
-let ser_wit_deprecated_number_modifier = let module M = Ser_genarg.GS(A1) in M.genser
-
 module A2 = struct
   type raw = Ser_number.number_option
   [@@deriving sexp,hash,compare]
@@ -74,7 +63,6 @@ end
 let ser_wit_string_option = let module M = Ser_genarg.GS(A6) in M.genser
 
 let register () =
-  Ser_genarg.register_genser Number_string_notation_plugin.G_number_string.wit_deprecated_number_modifier ser_wit_deprecated_number_modifier;
   Ser_genarg.register_genser Number_string_notation_plugin.G_number_string.wit_number_modifier ser_wit_number_modifier;
   Ser_genarg.register_genser Number_string_notation_plugin.G_number_string.wit_number_options ser_wit_number_options;
   Ser_genarg.register_genser Number_string_notation_plugin.G_number_string.wit_number_string_mapping ser_wit_number_string_mapping;
