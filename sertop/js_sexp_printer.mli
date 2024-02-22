772585322adc7d8eba42c20d62f7c55f78478be0
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-02-22 15:59:41 +0100

    [meta] Fix licence headers for JS files
    Thanks to Julien Puydt

--- sertop/js_sexp_printer.mli
+++ sertop/js_sexp_printer.mli
@@ -1,19 +1,28 @@
-(************************************************************************)
-(*         *   The Coq Proof Assistant / The Coq Development Team       *)
-(*  v      *         Copyright INRIA, CNRS and contributors             *)
-(* <O___,, * (see version control and CREDITS file for authors & dates) *)
-(*   VV/  **************************************************************)
-(*    //   *    This file is distributed under the terms of the         *)
-(*         *     GNU Lesser General Public License Version 2.1          *)
-(*         *     (see LICENSE file for the text of the license)         *)
-(************************************************************************)
+(** Emilio J. Gallego Arias: Code below is adapted from Jane Street's
+    sexplib, licence is:
 
-(************************************************************************)
-(* SerAPI: Coq interaction protocol with bidirectional serialization    *)
-(************************************************************************)
-(* Copyright 2016-2019 MINES ParisTech -- License LGPL 2.1+             *)
-(* Copyright 2019-2023 Inria           -- License LGPL 2.1+             *)
-(* Written by: Emilio J. Gallego Arias and others                       *)
-(************************************************************************)
+The MIT License
+
+Copyright (c) 2005--2024 Jane Street Group, LLC opensource-contacts@janestreet.com
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*)
 
 val pp_sertop : Format.formatter -> Sexplib.Sexp.t -> unit
